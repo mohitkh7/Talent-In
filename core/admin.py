39b6fd7323305a24ffbd7f241bd54f700f464729
Mohit Khandelwal <mohitkh7@gmail.com>
2018-12-02 11:11:46 +0530

    year and branch model created

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Year, branch
 # Register your models here.
+admin.site.register(Year)
+admin.site.register(Branch)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Year(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    value = models.IntegerField(unique=True)
+
+class Branch(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    short_name = models.CharField(max_length=5, unique=True)
