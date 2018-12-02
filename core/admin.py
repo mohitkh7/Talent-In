e2d66c72403c7b850367f5d515a069f60cea58df
Mohit Khandelwal <mohitkh7@gmail.com>
2018-12-02 11:28:32 +0530

    category model introduced for skill

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Year, Branch, Skill
+from .models import Year, Branch, Skill, Category
 # Register your models here.
 admin.site.register(Year)
 admin.site.register(Branch)
 admin.site.register(Skill)
+admin.site.register(Category)
--- core/models.py
+++ core/models.py
@@ -9,5 +9,16 @@ class Branch(models.Model):
     name = models.CharField(max_length=100, unique=True)
     short_name = models.CharField(max_length=5, unique=True)
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Skill(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE, default=5)
+
+    def __str__(self):
+        return self.name
+
