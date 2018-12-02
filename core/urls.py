1261f754b811c6bd0d4273400e51e2aa64ade988
Mohit Khandelwal <mohitkh7@gmail.com>
2018-12-02 10:58:40 +0530

    hackathon template file copied

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-"""SearchIn URL Configuration
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('core.urls')),
+    path('', views.index, name="home"),
+    path('login/', views.user_login, name="login"),
+    path('logout/', views.user_logout, name="logout"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 # Create your views here.
+def index(request):
+    # return HttpResponse("Om Gang Ganpataye Namah")
+    return render(request,'base.html')
+
+def user_login(request):
+    """
+    Handle the login logic for application
+    """
+    if request.method == "POST":
+        # Login
+        if "submit-login-form" in request.POST:
+            print(request.POST)
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            print(user)
+            if user is not None:
+
+                login(request, user)
+                return redirect('home')
+            else:
+                print("Error")
+        # Register
+        if "submit-register-form" in request.POST:
+            username = request.POST.get('username')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            if not (User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists()):
+                User.objects.create_user(username, email, password)
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    return render(request, 'login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
